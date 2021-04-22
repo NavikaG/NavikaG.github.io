ac0ff8aa90aa39d930b619593578502210293ace
Navika Gupta <navika@Navikas-MacBook-Pro.local>
2021-04-21 22:40:23 -0700

    done?

---
+++ typingAnim.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    var typed = new Typed(".typing", {
+       strings: [ "Student", "Learner","Programmer"],
+       typeSpeed: 100,
+       backSpeed: 60,
+       loop: true,
+       cursorChar: '|'
+    });
+    });
+    
